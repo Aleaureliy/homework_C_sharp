ceb10b78533cfc71ec32d9462647b33a998714e1
Aleaureliy <easternevil@mail.ru>
2023-09-29 20:16:44 +0300

    домашка к 7 семинару

--- homework7.2/Program.cs
+++ homework7.2/Program.cs
@@ -2,3 +2,40 @@
 // принимает позиции элемента в двумерном массиве, и
 // возвращает значение этого элемента или же указание,
 // что такого элемента нет.
+
+int[,] GetArray(int row, int colum, int minValue, int maxValue)
+{
+int[,] result = new int[row, colum];
+Random rand = new Random();
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < colum; j++)
+        {
+            result[i,j] = rand.Next(minValue, maxValue);
+        }
+    }
+return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void checkArray (int[,] array, int x, int y)
+{
+    if(x >= 0 && x <= array.GetLength(0) && y >= 0 && y <= array.GetLength(1)) Console.WriteLine($"Элемент по индексам {x} и {y} : {array[x, y]}");
+    else Console.WriteLine($"Элемента по индексами {x} и {y} нет.");
+}
+
+int[,] array = GetArray(3, 4, -5, 5);
+Console.WriteLine("Полученный массив: ");
+PrintArray(array);
+checkArray(array, 1, 1);
--- homework7.3/Program.cs
+++ homework7.3/Program.cs
@@ -1,3 +1,62 @@
 ﻿// Задача 52: Задайте двумерный массив из целых чисел.
 // Найдите среднее арифметическое элементов в каждом
 // столбце.
+
+int[,] GetArray(int row, int column, int minValue, int maxValue)
+{
+    Random rand = new Random();
+    int[,] array = new int[row, column];
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rand.Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void PrintArray1 (int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }   
+}
+
+void PrintArray2 (double[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i].ToString("F2") + "\t");
+    }
+}
+
+double[] SumArray(int[,] array)
+{
+    int size = array.GetLength(1);
+    double[] result = new double[size];
+    double sum = 0.0;
+    for(int i = 0; i < array.GetLength(1); i++)
+    {
+        double temp = 0.0;
+        for(int j = 0; j < array.GetLength(0); j++)
+        {
+            temp = temp + array[j, i];
+        }
+        sum = temp / array.GetLength(0);
+        result[i] = sum;
+    }
+    return result;
+}
+
+int[,] matrix = GetArray(3, 3, -5, 5);
+Console.WriteLine("Созданный массив: ");
+PrintArray1(matrix);
+double[] array = SumArray(matrix);
+Console.WriteLine("Среднее арифметическое столбцов: ");
+PrintArray2(array);
